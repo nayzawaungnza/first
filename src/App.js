92872f0ed2c2d1cf2956ef66c0f7842ae72e336b
nayzawaung750nza <nayzawaung750@gmail.com>
2023-09-19 10:09:38 +0630

    step 7 State

--- src/App.js
+++ src/App.js
@@ -10,13 +10,23 @@ class Item extends React.Component{
   }
 }
 class App extends React.Component{
+  state = {
+    items:[
+      {id:1, name:'Apple', price:0.99},
+      {id:2, name:'Orange', price:0.88},
+      {id:2, name:'Seed', price:0.77}
+    ]
+  };
   render(){
     return (
       <div>
         <h1>Hello React</h1>
         <ul>
-          <Item name="Apple" price="0.99" />
-          <Item name="Orange" price="0.88" />
+          {this.state.items.map(i=>{
+            return(
+              <Item name={i.name} price={i.price}></Item>
+            )
+          })}
         </ul>
       </div>
 
